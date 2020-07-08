50ce71cfc6ce5fa6bee52770e161ab316ed2148d
LieutenantCrunch <52416950+LieutenantCrunch@users.noreply.github.com>
2020-07-07 23:41:25 -0400

    Serve client files
    Integrated FileHandler and can now serve client files.

--- server/runtime/app.js
+++ server/runtime/app.js
@@ -7,54 +7,76 @@ require("reflect-metadata");
 const express_1 = __importDefault(require("express"));
 const typeorm_1 = require("typeorm");
 const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const User_1 = require("./entity/User");
+const filehandler_1 = __importDefault(require("./utilities/filehandler"));
 const app = express_1.default();
 const port = 3000;
 var dbConnection;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const fileHandler = new filehandler_1.default();
+app.get(/^\/(index)?$/, (req, res) => {
+    fileHandler.sendFileResponse(res, '../client/public/index.html', 'text/html');
 });
-app.get('/checkdb', (req, res) => {
-    fs_1.default.readFile('./private/dbpass.txt', 'utf8', (readFileError, data) => {
-        if (readFileError) {
-            console.error(readFileError);
-            res.send('Couldn\'t find the password');
-        }
-        typeorm_1.createConnection({
-            type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'nodejs',
-            password: data.trim(),
-            database: 'scrapbook_dev',
-            synchronize: true,
-            logging: false,
-            entities: [
-                __dirname + '/entity/*.js'
-            ]
-        }).then((connection) => {
-            dbConnection = connection;
-            res.send('Successfully connected to database');
-        }).catch((error) => {
-            console.error(error);
-            res.send('Couldn\'t connect to the database');
-        });
-    });
+app.get(/.css$/, (req, res) => {
+    const cssFileName = path_1.default.resolve('../client', req.path.substr(1));
+    fileHandler.sendFileResponse(res, cssFileName, 'text/css');
 });
-app.get('/listusers', async (req, res) => {
-    if (dbConnection === undefined || dbConnection === null) {
-        res.send('No database connection found');
+app.get(/\.js$/, (req, res) => {
+    const jsFileName = path_1.default.resolve('../client', req.path.substr(1));
+    fileHandler.sendFileResponse(res, jsFileName, 'text/javascript');
+});
+app.get('/api/:methodName', async (req, res) => {
+    switch (req.params.methodName) {
+        case 'check-db':
+            fs_1.default.readFile('./private/dbpass.txt', 'utf8', (readFileError, data) => {
+                if (readFileError) {
+                    console.error(readFileError);
+                    res.send('Couldn\'t find the password');
+                }
+                typeorm_1.createConnection({
+                    type: 'mysql',
+                    host: 'localhost',
+                    port: 3306,
+                    username: 'nodejs',
+                    password: data.trim(),
+                    database: 'scrapbook_dev',
+                    synchronize: true,
+                    logging: false,
+                    entities: [
+                        __dirname + '/entity/*.js'
+                    ]
+                }).then((connection) => {
+                    dbConnection = connection;
+                    res.send('Successfully connected to database');
+                }).catch((error) => {
+                    console.error(error);
+                    res.send('Couldn\'t connect to the database');
+                });
+            });
+            break;
+        case 'list-users':
+            if (dbConnection === undefined || dbConnection === null) {
+                res.send('No database connection found');
+            }
+            let userRepository = dbConnection.getRepository(User_1.User);
+            let allUsers = await userRepository.find();
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            allUsers.forEach((user) => {
+                res.write(user.displayName);
+            });
+            res.end();
+            break;
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.write('Not Found');
+            res.end();
+            break;
     }
-    let userRepository = dbConnection.getRepository(User_1.User);
-    let allUsers = await userRepository.find();
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    allUsers.forEach((user) => {
-        res.write(user.displayName);
-    });
-    res.end();
 });
-app.get('/:test', (req, res) => {
-    res.send(`Hello ${req.param('test', 'World.')}!`);
+app.use((req, res) => {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.write('Not Found');
+    res.end();
 });
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,65 +1,93 @@
 import 'reflect-metadata';
 import express, {Request, Response} from 'express';
 import {createConnection, Any, Connection, Repository} from 'typeorm';
+
 import fs from 'fs';
+import path from 'path';
+
 import {User} from './entity/User';
+import FileHandler from './utilities/filehandler'
 
 const app: express.Application = express();
 const port: number = 3000;
 var dbConnection: Connection;
+const fileHandler: FileHandler = new FileHandler();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+app.get(/^\/(index)?$/, (req: Request, res: Response) => {
+    fileHandler.sendFileResponse(res, '../client/public/index.html', 'text/html');
 });
 
-app.get('/checkdb', (req: Request, res: Response) => {
-    fs.readFile('./private/dbpass.txt', 'utf8', (readFileError: NodeJS.ErrnoException | null, data: string) => {
-        if (readFileError) {
-            console.error(readFileError);
-            res.send('Couldn\'t find the password');
+app.get(/.css$/, (req: Request, res: Response) => {
+    const cssFileName: string = path.resolve('../client', req.path.substr(1));
+
+    fileHandler.sendFileResponse(res, cssFileName, 'text/css');
+});
+
+app.get(/\.js$/, (req: Request, res: Response) => {
+    const jsFileName: string = path.resolve('../client', req.path.substr(1));
+
+    fileHandler.sendFileResponse(res, jsFileName, 'text/javascript');
+});
+
+app.get('/api/:methodName', async (req: Request, res: Response) => {
+    switch (req.params.methodName)
+    {
+    case 'check-db':
+        fs.readFile('./private/dbpass.txt', 'utf8', (readFileError: NodeJS.ErrnoException | null, data: string) => {
+            if (readFileError) {
+                console.error(readFileError);
+                res.send('Couldn\'t find the password');
+            }
+    
+            createConnection({
+                type: 'mysql',
+                host: 'localhost',
+                port: 3306,
+                username: 'nodejs',
+                password: data.trim(),
+                database: 'scrapbook_dev',
+                synchronize: true,
+                logging: false,
+                entities: [
+                    __dirname + '/entity/*.js'
+                ]
+            }).then((connection: Connection) => {
+                dbConnection = connection;
+                res.send('Successfully connected to database');
+            }).catch((error: any) => {
+                console.error(error);
+                res.send('Couldn\'t connect to the database');
+            });
+        })
+        break;
+    case 'list-users':
+        if (dbConnection === undefined || dbConnection === null) {
+            res.send('No database connection found');
         }
-
-        createConnection({
-            type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'nodejs',
-            password: data.trim(),
-            database: 'scrapbook_dev',
-            synchronize: true,
-            logging: false,
-            entities: [
-                __dirname + '/entity/*.js'
-            ]
-        }).then((connection: Connection) => {
-            dbConnection = connection;
-            res.send('Successfully connected to database');
-        }).catch((error: any) => {
-            console.error(error);
-            res.send('Couldn\'t connect to the database');
+    
+        let userRepository: Repository<User> = dbConnection.getRepository(User);
+        let allUsers: User[] = await userRepository.find();
+    
+        res.writeHead(200, {'Content-Type': 'text/html'});
+    
+        allUsers.forEach((user: User) => {
+            res.write(user.displayName);
         });
-    })
-});
-
-app.get('/listusers', async (req: Request, res: Response) => {
-    if (dbConnection === undefined || dbConnection === null) {
-        res.send('No database connection found');
+    
+        res.end();
+        break;
+    default:
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.write('Not Found');
+        res.end();
+        break;
     }
-
-    let userRepository: Repository<User> = dbConnection.getRepository(User);
-    let allUsers: User[] = await userRepository.find();
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
-
-    allUsers.forEach((user: User) => {
-        res.write(user.displayName);
-    });
-
-    res.end();
 });
 
-app.get('/:test', (req: Request, res: Response) => {
-    res.send(`Hello ${req.param('test', 'World.')}!`);
+app.use((req: Request, res: Response) => {
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    res.write('Not Found');
+    res.end();
 });
 
 app.listen(port, () => {
