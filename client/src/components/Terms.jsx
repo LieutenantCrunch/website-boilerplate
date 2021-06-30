e270432cf1d30049c62649878bbe0f885e073b43
LieutenantCrunch <52416950+LieutenantCrunch@users.noreply.github.com>
2021-06-30 14:23:19 -0400

    Added site comparisons
    Added notes about how this site is better than facebook and twitter
    Tweaked the ToS

--- client/src/components/Terms.jsx
+++ client/src/components/Terms.jsx
@@ -7,29 +7,32 @@ export const Terms = ({ setTitle }) => {
     return (
         <div className="col-12 col-sm-10 col-md-8 col-lg-6 col-xxl-4 mt-2 align-middle text-left">
             <div className="mb-2">
-                So you want to use my website, huh? Well, I know that nobody really reads Terms of Service anyway, so I'll try to keep it simple.
+                So you want to use my website, huh? Well, I know that nobody really reads Terms of Service anyway, so I'll try to keep them simple.
                 If you register for my website, you agree to the following:
             </div>
             <ul className="list-group">
                 <li className="list-group-item">
-                    You will not hold me legally responsible for anything related to this website.
+                    You will not hold me legally responsible for anything related to this website (or whatever you could possibly come up with).
                     I just built this thing to improve my skills so I could get a job. There's no security team to ensure your data is safe or that best practices are being followed 100%.
                 </li>
                 <li className="list-group-item">
-                    Don't use a password for this website that you use elsewhere.
+                    You will not use a password for this website that you use elsewhere.
                     Passwords are encrypted and are not stored in plain text, but it's just general bad practice to reuse passwords.
                 </li>
                 <li className="list-group-item">
                     I reserve the right to delete anything I find on this website.
                 </li>
                 <li className="list-group-item">
-                    Don't post anything here you wouldn't want the entire world to see.
+                    You will not post anything here you wouldn't want the entire world to see.
                 </li>
                 <li className="list-group-item">
-                    Don't post stuff you don't have the rights to post (i.e. mp3s/videos/whatever that could get you or me nailed with a copyright notice).
+                    You will not post stuff you don't have the rights to post (i.e. mp3s/videos/whatever that could get you or me nailed with a copyright notice).
                 </li>
                 <li className="list-group-item">
-                    Don't use this site to launch a nuclear missile. I don't know how you would, but that seems to be a popular thing to put in terms of service.
+                    You will not use this site to launch a nuclear missile. I don't know how you would, but that seems to be a popular thing to put in terms of service.
+                </li>
+                <li className="list-group-item">
+                    You understand that these Terms of Service could change at some point in the future.
                 </li>
             </ul>
         </div>
